9a063fcf418a61b8a922094f21f6e8f36a4415be
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-06-15 13:07:03 -0400

    cvPCA allows both train and test.

--- power_law_rdm/utils_powerlaw.py
+++ power_law_rdm/utils_powerlaw.py
@@ -3,37 +3,42 @@ from scipy.optimize import curve_fit
 from sklearn.decomposition import PCA
 
 
-def shuff_cvPCA(X, another, nshuff=5, seed=942):
+def run_cvPCA(X, train=None, nshuff=5, seed=942):
     """
     :param X: 2 x neu x time
-    :param another: neu x time
+    :param train: neu x time
     :param nshuff:
     :return:
     """
     np.random.seed(seed)
-    nc = min(1024, X.shape[2])
-    assert another.shape[1] >= nc  # t
-    assert X.shape[1] == another.shape[0]  # neu
+    n_components = min(1024, X.shape[2])
 
-    ss = np.zeros((nshuff, nc))
+    if train is not None:
+        assert train.shape[1] >= n_components  # t
+        assert X.shape[1] == train.shape[0]  # neu
+
+    ss = np.zeros((nshuff, n_components))
     for k in range(nshuff):
         print(f'cvPCA Iter {k}')
-        idx_other = np.random.rand(another.shape[1]) > 0.5  # Choose time.
 
         idx_flip = np.random.rand(X.shape[2]) > 0.5  # Bootstrap 50%.
         X_use = X.copy()
         X_use[0, :, idx_flip] = X[1, :, idx_flip]
         X_use[1, :, idx_flip] = X[0, :, idx_flip]
 
-        ss[k, :] = cvPCA(X_use, another[:, idx_other], nc)
+        if train is None:
+            ss[k, :] = _cvPCA(X_use, X_use[0, ...], n_components)
+        else:
+            idx_other = np.random.rand(train.shape[1]) > 0.5  # Choose time.
+            ss[k, :] = _cvPCA(X_use, train[:, idx_other], n_components)
 
     return ss
 
 
-def cvPCA(X, another, nc):
-    model = PCA(n_components=nc).fit(another)  # neu x stim ; pcs are 'supertime'
+def _cvPCA(X, train, nc):
+    model = PCA(n_components=nc).fit(train)  # neu x stim ; pcs are 'supertime'
 
-    xproj = another @ (model.components_.T / model.singular_values_)
+    xproj = train @ (model.components_.T / model.singular_values_)
     cproj0 = X[0, ...].T @ xproj  # Get component in that direction.
     cproj1 = X[1, ...].T @ xproj
 
