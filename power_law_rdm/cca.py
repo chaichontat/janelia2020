78335550532642dd687067d4e8cf655e1b16824a
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-06-19 18:29:09 -0400

    Add docs to CCA.

--- power_law_rdm/cca.py
+++ power_law_rdm/cca.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import jax.numpy as np
 import numpy as onp
 from jax.numpy.linalg import inv, cholesky
@@ -5,15 +7,26 @@ from sklearn.decomposition import PCA
 
 from spikeloader import SpikeLoader
 
+Arrays = Union[np.DeviceArray, onp.ndarray]
+
 
 class CanonicalRidge:
-    def __init__(self, n=25, lx=0.95, ly=0.95):
+    """
+    Class for canonical correlation analysis with ridge regularization.
+    See http://www2.imm.dtu.dk/pubdb/edoc/imm4981.pdf
+
+    Uses JAX for faster numerical computation.
+    Based on the scikit-learn Estimator API.
+
+    """
+
+    def __init__(self, n: int = 25, lx: float = 0.95, ly: float = 0.95):
         self.n = n
         self.λx = lx
         self.λy = ly
         self.V, self.singular_values, self.Σ, self.U = 4 * [None]
 
-    def fit(self, X, Y):
+    def fit(self, X: Arrays, Y: Arrays):
         K = self._calc_canon_mat(X, Y)
         model = PCA(self.n).fit(K)  # Randomized SVD.
 
@@ -23,37 +36,41 @@ class CanonicalRidge:
         self.U = K @ self.V @ np.linalg.inv(self.Σ)
         return self
 
-    def fit_transform(self, X, Y):
+    def fit_transform(self, X: Arrays, Y: Arrays) -> np.DeviceArray:
         self.fit(X, Y)
         return self.transform(X, Y)
 
-    def transform(self, X, Y):
+    def transform(self, X: Arrays, Y: Arrays) -> np.DeviceArray:
         return X @ self.U, Y @ self.V
 
-    def calc_canon_coef(self, X, Y):
+    def calc_canon_coef(self, X: Arrays, Y: Arrays) -> np.DeviceArray:
         X_p, Y_p = self.transform(X, Y)
         return np.array([onp.corrcoef(X_p[:, i], Y_p[:, i])[0, 1] for i in range(self.n)])
 
-    def _calc_canon_mat(self, X, Y):
+    def _calc_canon_mat(self, X: Arrays, Y: Arrays) -> np.DeviceArray:
         X, Y = np.asarray(X), np.asarray(Y)
 
-        # Center
         X -= np.mean(X, axis=0)
         Y -= np.mean(Y, axis=0)
-
-        # See http://www2.imm.dtu.dk/pubdb/edoc/imm4981.pdf
         K = inv(cholesky((1 - self.λx) * np.cov(X, rowvar=False) + self.λx * np.eye(X.shape[1]))) @ \
             (X.T @ Y) / (X.T.shape[0] * Y.shape[1]) @ \
             inv(cholesky((1 - self.λy) * np.cov(Y, rowvar=False) + self.λy * np.eye(Y.shape[1])))
+
         return K
 
 
-if __name__ == '__main__':
-    data = SpikeLoader('superstim.npz')
-    split = data.S_corr.shape[1] // 2
+def prepare_data(path='superstim.npz'):
+    data = SpikeLoader(path)
+    split = onp.random.rand(data.S_corr.shape[1]) > 0.5
+    inv_split = np.in1d(np.arange(data.S_corr.shape[1]), split, assume_unique=True, invert=True)
 
-    train = [data.S_corr[data.ypos >= 210, :split].T, data.S_corr[data.ypos < 210, :split].T]
-    test = [data.S_corr[data.ypos >= 210, split:].T, data.S_corr[data.ypos < 210, split:].T]
+    train = [data.S_corr[data.ypos >= 210][:, split].T, data.S_corr[data.ypos < 210][:, split].T]
+    test = [data.S_corr[data.ypos >= 210][:, inv_split].T, data.S_corr[data.ypos < 210][:, inv_split].T]
+    return train, test
+
+
+if __name__ == '__main__':
+    train, test = prepare_data()
     X, Y = np.asarray(train[0]), np.asarray(train[1])
 
     # %%
--- power_law_rdm/cca_cv.py
+++ power_law_rdm/cca_cv.py
@@ -1,22 +1,18 @@
 from functools import partial
 
 import jax.numpy as np
-import numpy as onp
+import matplotlib.pyplot as plt
+import seaborn as sns
 from skopt import gp_minimize
+from skopt import plots
 from skopt.space import Real
 
-from cca import CanonicalRidge
-from spikeloader import SpikeLoader
+from cca import CanonicalRidge, prepare_data
 
 """ 2-fold CV for hyperparameter optimization using Gaussian processes. """
 
-# %% Prepare data.
-data = SpikeLoader('superstim.npz')
-split = onp.random.rand(data.S_corr.shape[1]) > 0.5
-inv_split = np.in1d(np.arange(data.S_corr.shape[1]), split, assume_unique=True, invert=True)
-
-train = [data.S_corr[data.ypos >= 210][:, split].T, data.S_corr[data.ypos < 210][:, split].T]
-test = [data.S_corr[data.ypos >= 210][:, inv_split].T, data.S_corr[data.ypos < 210][:, inv_split].T]
+sns.set()
+train, test = prepare_data()
 
 
 # %% Run optimization
@@ -27,9 +23,8 @@ def objective(train, test, *λ):
     tr = -np.mean(model.calc_canon_coef(*test))
 
     model = CanonicalRidge(lx=λ[0], ly=λ[1]).fit(*test)
-    tr -= np.mean(model.calc_canon_coef(*train))
+    tr += -np.mean(model.calc_canon_coef(*train))
     return float(tr)
-
 
 space = [
     Real(0.8, 0.9999, name='λx'),
@@ -41,9 +36,6 @@ res_gp = gp_minimize(func, space, n_calls=20, n_random_starts=5, random_state=43
                      verbose=True)
 
 # %%
-from skopt import plots
-import matplotlib.pyplot as plt
-
 plt.rcParams['figure.dpi'] = 300
 plots.plot_objective(res_gp, size=4)
 plt.suptitle('Gaussian Process λ tuning. \n'
