b165568b568d683e85e9919ea8ada44746a72777
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-06-18 22:02:44 -0400

    Refactor data load into class.

--- power_law_rdm/utils_powerlaw.py
+++ power_law_rdm/utils_powerlaw.py
@@ -1,76 +1,33 @@
 import numpy as np
-from scipy.optimize import curve_fit
+from scipy.stats import zscore
 from sklearn.decomposition import PCA
 
 
-def run_cvPCA(X, train=None, nshuff=5, seed=942, dim='stim'):
-    """
-    :param X: 2 x neu x time
-    :param train: neu x time
-    :param nshuff:
-    :return:
-    """
-    np.random.seed(seed)
-    n_components = min(1024, X.shape[2])
+class Data:
+    def __init__(self, path: str = '../superstim.npz'):
+        # %% Pre-processing
+        print('Loading data.')
+        with np.load(path) as npz:
+            self.img, self.spks = npz['img'], npz['spks']
+            self.xpos, self.ypos = npz['xpos'], npz['ypos']
+            self.frame_start, self.istim = npz['frame_start'], npz['istim']
 
-    ss = np.zeros((nshuff, n_components))
-    for k in range(nshuff):
-        print(f'cvPCA Iter {k}')
+        assert len(self.frame_start) == len(self.istim)
+        self.S = zscore(self.spks[:, self.frame_start], axis=1)  # neu x time
+        self.S_corr = self.remove_spont(self.S)
 
-        idx_flip = np.random.rand(X.shape[2]) > 0.5  # Flip stims. Bootstrap 50%.
-        X_use = X.copy()
-        X_use[0, :, idx_flip] = X[1, :, idx_flip]
-        X_use[1, :, idx_flip] = X[0, :, idx_flip]
+    def remove_spont(self, S):
+        idx_spont = \
+            np.where(
+                np.isin(np.arange(np.max(self.frame_start) + 1), self.frame_start, assume_unique=True, invert=True))[
+                0]  # Invert indices.
+        assert idx_spont.size + self.frame_start.size == self.spks.shape[1]
 
-        if dim == 'stim':
-            if train is not None:
-                assert train.shape[1] >= n_components  # t
-                assert train.shape[0] == X.shape[1]  # neu
-                train_ = train
+        S_spont = zscore(self.spks[:, idx_spont], axis=1)  # neu x time
 
-        elif dim == 'V1':  # Transpose X and train. -> PCs have dim
-            if train is not None:
-                # assert train.shape[2] == X.shape[2]
-                V1 = X_use[:, ypos >= 210, :]
-                V2 = X_use[:, ypos < 210, :]
-                train_ = V2[0, ...].T
-                X_use = np.transpose(V1, axes=(0, 2, 1))
-            else:
-                X_use = X_use[:, ypos >= 210, :]
+        pca_spont = PCA(n_components=25).fit(S_spont.T)  # time x neu ; pcs are 'superneurons'
+        pcs_spont = pca_spont.components_  # n_components x neu
 
-        elif dim == 'V2':  # Transpose X and train. -> PCs have dim
-            if train is not None:
-                # assert train.shape[2] == X.shape[2]
-                V1 = X_use[:, ypos >= 210, :]
-                V2 = X_use[:, ypos < 210, :]
-                train_ = V1[0, ...].T
-                X_use = np.transpose(V2, axes=(0, 2, 1))
-            else:
-                X_use = X_use[:, ypos < 210, :]
-        else:
-            raise Exception('What.')
-
-
-        if train is None:
-            ss[k, :] = _cvPCA(X_use, X_use[0, ...], n_components)
-        else:
-            idx_other = np.random.rand(train_.shape[1]) > 0.5  # Choose time.
-            ss[k, :] = _cvPCA(X_use, train_[:, idx_other], n_components)
-
-    return ss
-
-def _cvPCA(X, train, n_components):
-    assert X.shape[1] == train.shape[0]
-    model = PCA(n_components=n_components).fit(train.T)  # X = UΣV^T
-    # Generate 'super-neurons'
-    comp = model.components_.T  # n_components x neu
-    # Rotate entire dataset and extract first {n_components} dims, aka low-rank descriptions of neuronal activities.
-    # Then calculate inner products between {n_components} stim vectors, aka covariance.
-    return np.sum((X[0, ...].T @ comp) * (X[1, ...].T @ comp), axis=0)
-
-def fit_powerlaw(ss, dmin=50, dmax=500):
-    def power_law(x, k, α):
-        return k * x ** α
-
-    popt, pcov = curve_fit(power_law, np.arange(dmin, dmax), ss[dmin:dmax])
-    return popt, pcov
+        proj_spont = S.T @ pcs_spont.T  # neu x n_components
+        S_corr = zscore((S.T - proj_spont @ pcs_spont).T, axis=1)  # neu x time
+        return S_corr
