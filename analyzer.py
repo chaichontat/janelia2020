329f47e975a7bd7dcb904908cf2fc986677fed23
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-06-25 17:19:12 -0400

    Clean up power law.

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,10 @@
+import numpy as np
+from scipy.stats import zscore
+from sklearn.decomposition import PCA
+
+from spikeloader import SpikeLoader
+
+
 class Analyzer:
     """
     Abstract class for data analysis from raw spike data in the form of `SpikeLoader` instance.
@@ -17,3 +24,48 @@ class Analyzer:
         except KeyError:
             pass
         return d
+
+
+class SubtractSpontAnalyzer(Analyzer):
+    def __init__(self, loader: SpikeLoader, n_spont_pc: int = 25):
+        self.loader = loader
+        self.n_spont_pc = n_spont_pc
+        if self.n_spont_pc > 0:
+            print(f'Subtracting {n_spont_pc} spontaneous components.')
+            self.S_nospont = self.subtract_spont(self.loader.S)
+        else:
+            print(f'No spontaneous subtraction. `self.S_nospont` not loaded.')
+            self.S_nospont = self.loader.S
+
+    def subtract_spont(self, S: np.ndarray) -> np.ndarray:
+        """
+        Project S onto the spontaneous activities subspace and subtract out.
+
+        Parameters
+        ----------
+        S: np.ndarray
+            Non-spontaneous spiking data (stim x neu).
+        n_components: int
+            Number of spontaneous PCs to subtract.
+
+        Returns
+        -------
+        S_corr: np.ndarray
+            S after subtraction
+
+        """
+        idx_spont = \
+            np.where(np.isin(np.arange(np.max(self.loader.frame_start) + 1), self.loader.frame_start,
+                             assume_unique=True, invert=True))[0]  # Invert indices.
+        assert idx_spont.size + self.loader.frame_start.size == self.loader.spks.shape[0]
+
+        S_spont = zscore(self.loader.spks[idx_spont, :], axis=0)  # time x neu
+
+        n_used = min(3 * self.n_spont_pc, *S_spont.shape)  # Randomized SVD.
+        pca_spont = PCA(n_components=n_used).fit(S_spont)  # time x neu
+        pcs_spont = pca_spont.components_.T[:, :self.n_spont_pc]  # neu x n_components
+
+        proj_spont = S @ pcs_spont  # neu x n_components
+        self.S_nospont = zscore(S - proj_spont @ pcs_spont.T, axis=0)
+
+        return self.S_nospont
--- power_law_rdm/cvpca.py
+++ power_law_rdm/cvpca.py
@@ -6,67 +6,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from scipy.optimize import curve_fit
-from scipy.stats import zscore
 from sklearn.decomposition import PCA
 
-from analyzer import Analyzer
+from analyzer import SubtractSpontAnalyzer
 from spikeloader import SpikeStimLoader
 
 
 # %%
-class cvPCA(Analyzer):
+class cvPCA(SubtractSpontAnalyzer):
     def __init__(self, loader: SpikeStimLoader, n_spont_pc: int = 25,
                  n_cvpc: int = 1024, n_shuff: int = 5, seed: int = 124):
-        self.loader = loader
+        super().__init__(loader, n_spont_pc)
         self.frame_start, self.istim = self.loader.frame_start, self.loader.istim
 
-        self.n_spont_pc = n_spont_pc
         self.n_cvpc = n_cvpc
         self.n_shuff = n_shuff
         self.seed = seed
 
-        if self.n_spont_pc > 0:
-            print(f'Subtracting {n_spont_pc} spontaneous components.')
-            self.S_nospont = self.subtract_spont(self.loader.S)
-        else:
-            print(f'No spontaneous subtraction. `self.S_nospont` not loaded.')
-            self.S_nospont = self.loader.S
         self.X = np.empty(0)
         self.Y = np.empty(0)
         self.sums_of_squares = np.empty(0)
-
-    def subtract_spont(self, S: np.ndarray) -> np.ndarray:
-        """
-        Project S onto the spontaneous activities subspace and subtract out.
-
-        Parameters
-        ----------
-        S: np.ndarray
-            Non-spontaneous spiking data (stim x neu).
-        n_components: int
-            Number of spontaneous PCs to subtract.
-
-        Returns
-        -------
-        S_corr: np.ndarray
-            S after subtraction
-
-        """
-        idx_spont = \
-            np.where(np.isin(np.arange(np.max(self.loader.frame_start) + 1), self.loader.frame_start,
-                             assume_unique=True, invert=True))[0]  # Invert indices.
-        assert idx_spont.size + self.loader.frame_start.size == self.loader.spks.shape[0]
-
-        S_spont = zscore(self.loader.spks[idx_spont, :], axis=0)  # time x neu
-
-        n_used = min(3 * self.n_spont_pc, *S_spont.shape)  # Randomized SVD.
-        pca_spont = PCA(n_components=n_used).fit(S_spont)  # time x neu
-        pcs_spont = pca_spont.components_.T[:, :self.n_spont_pc]  # neu x n_components
-
-        proj_spont = S @ pcs_spont  # neu x n_components
-        self.S_nospont = zscore(S - proj_spont @ pcs_spont.T, axis=0)
-
-        return self.S_nospont
 
     def gen_cvpca_format(self, S_nospont=None):
         if S_nospont is not None:
