25da40ff5b106d2a1eee93b9ccd51cae32b292ab
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-06-26 15:53:42 -0400

    Update SpikeLoader.

--- src/spikeloader.py
+++ src/spikeloader.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from typing import Tuple
 
 import numpy as np
@@ -12,44 +13,63 @@ class SpikeLoader:
     Primary 'raw' data storage and pre-processing before analysis.
     """
 
-    def __init__(self, path: str = 'superstim.npz'):
+    def __init__(self, path: str = 'superstim32.npz', img_scale: float = 0.25):
         print('Loading data.')
         self.path = path
+        self.img_scale = img_scale
+
         with np.load(self.path, mmap_mode='r') as npz:
-            self._spks = np.empty(0)  # Load on demand.
             self.xpos, self.ypos = npz['xpos'], npz['ypos']
             self.frame_start, self.istim = npz['frame_start'], npz['istim']
 
         # Sanity checks.
         assert len(self.frame_start) == len(self.istim)  # Number of stimulations.
-
-        self.S = zscore(self.spks[self.frame_start, :], axis=0)
+        assert len(self.xpos) == len(self.ypos)
 
     @property
+    @lru_cache
+    def S(self):
+        return zscore(self.spks[self.frame_start, :], axis=0)
+
+    @property
+    @lru_cache
     def spks(self):
-        if len(self._spks) == 0:
-            with np.load(self.path, mmap_mode='r') as npz:
-                self._spks = npz['spks'].T  # time x neu
-            assert len(self.xpos) == len(self.ypos) == self._spks.shape[1]  # Number of neurons.
-        return self._spks
-
-    def train_test_split(self, test_size: float = 0.5, random_state: int = 1256) -> Tuple:
-        return train_test_split(self.S, test_size=test_size, random_state=random_state)
-
-
-class SpikeStimLoader(SpikeLoader):
-    def __init__(self, *args, img_scale: float = 0.25, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.img_scale = img_scale
-
         with np.load(self.path, mmap_mode='r') as npz:
-            self.img = np.transpose(npz['img'], (2, 0, 1))  # stim x y x x
+            return npz['spks'].T  # time x neu
 
-        self.img = ndi.zoom(self.img, (1, self.img_scale, self.img_scale), order=1)
+    @property
+    @lru_cache
+    def img(self):
+        with np.load(self.path, mmap_mode='r') as npz:
+            return ndi.zoom(np.transpose(npz['img'], (2, 0, 1)), (1, self.img_scale, self.img_scale), order=1)
 
-        # Normalized.
-        self.X = np.reshape(self.img[self.istim, ...], [len(self.istim), -1])
-        self.X = zscore(self.X, axis=0) / np.sqrt(len(self.istim))  # (stim x pxs)
+    @property
+    @lru_cache
+    def X(self):
+        X = np.reshape(self.img[self.istim, ...], [len(self.istim), -1])
+        return zscore(X, axis=0) / np.sqrt(len(self.istim))  # (stim x pxs)
 
     def train_test_split(self, test_size: float = 0.5, random_state: int = 1256) -> Tuple:
         return train_test_split(self.X, self.S, test_size=test_size, random_state=random_state)
+
+
+# class SpikeLoader(SpikeLoader):
+#     def __init__(self, *args, img_scale: float = 0.25, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.img_scale = img_scale
+#
+#         with np.load(self.path, mmap_mode='r') as npz:
+#             self.img = np.transpose(npz['img'], (2, 0, 1))  # stim x y x x
+#
+#         self.img = ndi.zoom(self.img, (1, self.img_scale, self.img_scale), order=1)
+#
+#         # Normalized.
+#         self.X = np.reshape(self.img[self.istim, ...], [len(self.istim), -1])
+#         self.X = zscore(self.X, axis=0) / np.sqrt(len(self.istim))  # (stim x pxs)
+#
+#     def train_test_split(self, test_size: float = 0.5, random_state: int = 1256) -> Tuple:
+#         return train_test_split(self.X, self.S, test_size=test_size, random_state=random_state)
+
+
+if __name__ == '__main__':
+    test = SpikeLoader()
