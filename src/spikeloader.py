ce284069abe9daf6e0917be97f5aa4fe6d50eb81
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-06-29 15:02:36 -0400

    Update HDF5 save/load.

--- src/spikeloader.py
+++ src/spikeloader.py
@@ -1,6 +1,7 @@
 import os
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Tuple, Union
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
@@ -9,33 +10,38 @@ from scipy import ndimage as ndi
 from scipy.stats import zscore
 from sklearn.model_selection import train_test_split
 
-os.environ['BLOSC_NTHREADS'] = '8'
+from .utils.utils import hdf5_load, hdf5_save_from_obj
 
+
+os.environ['BLOSC_NTHREADS'] = '8'
+Path_str = Union[str, Path]
 
 @dataclass
 class SpikeLoader:
-    path: str
-    pos: pd.DataFrame
-    istim: pd.Series
+    path: Path_str
+
+    pos: pd.DataFrame  # (n_neu ✕ 2)
+    istim: pd.Series  # (n_stim). Index is (t).
     img_scale: float
 
-    spks: np.ndarray = np.empty(0)
-    imgs: np.ndarray = np.empty(0)
+    spks: np.ndarray = np.empty(0)  # (t ✕ n_neu)
+    imgs: np.ndarray = np.empty(0)  # (n_img ✕ y ✕ x)
 
-    _S: np.ndarray = np.empty(0)
-    _imgs_stim: np.ndarray = np.empty(0)
-    _img_dim: np.ndarray = np.empty(0)
+    _S: np.ndarray = np.empty(0)  # (n_stim ✕ n_neu)
+    _imgs_stim: np.ndarray = np.empty(0)  # (n_stim ✕ n_px)
+    img_dim: np.ndarray = np.empty(0)  # (y, x)
 
     """ Anything with an `_` prefix means that it's a product of this class and 
     it is only there to save time. Not expected for a first run. """
 
     @classmethod
-    def from_npz(cls, path: str = 'data/superstim32.npz', img_scale: float = 0.25):
+    def from_npz(cls, path: Path_str = 'data/superstim32.npz', img_scale: float = 0.25):
         with np.load(path, mmap_mode='r') as npz:
             pos = pd.DataFrame({'x': npz['xpos'], 'y': npz['ypos']})
             istim = pd.Series(npz['istim'], index=npz['frame_start'])
             spks = npz['spks'].T
             imgs = ndi.zoom(np.transpose(npz['img'], (2, 0, 1)), (1, img_scale, img_scale), order=1)
+            img_dim = imgs.shape[1:]
         del npz
         return cls(**{k: v for k, v in locals().items() if k != 'cls'})
 
@@ -44,13 +50,6 @@ class SpikeLoader:
         if len(self._S) == 0:
             self._S = zscore(self.spks[self.istim.index, :], axis=0)
         return self._S
-
-    @property
-    def img_dim(self):
-        if len(self._img_dim) > 0:
-            assert len(self._img_dim) == 2
-            return self._img_dim
-        return self.imgs.shape[1:]
 
     @property
     def imgs_stim(self):
@@ -76,40 +75,26 @@ class SpikeLoader:
     def train_test_split(self, test_size: float = 0.5, random_state: int = 1256) -> Tuple:
         return train_test_split(self.imgs_stim, self.S, test_size=test_size, random_state=random_state)
 
-    def save(self, path='data/examples.hdf5'):
-        filters = tables.Filters(complib='blosc:lz4hc', complevel=5)
-        folder = type(self).__name__
-        with tables.open_file(path, 'w') as f:
-            group = f.create_group(f.root, folder)
-            f.create_carray(group, 'imgs', obj=self.imgs, filters=filters)
-            f.create_carray(group, 'spks', obj=self.spks, filters=filters)
-        self.istim.to_hdf(path, f'{folder}/istim')
-        self.pos.to_hdf(path, f'{folder}/pos')
+    def save(self, path: Path_str = 'data/examples.hdf5', overwrite=False):
+        arrs = ['imgs', 'spks']
+        dfs = ['istim', 'pos']
+        params = ['img_scale']
+        return hdf5_save_from_obj(path, 'SpikeLoader', self, arrs=arrs, dfs=dfs, params=params, overwrite=overwrite)
 
-    def save_processed(self, path='data/processed.hdf5'):
-        filters = tables.Filters(complib='blosc:lz4hc', complevel=5)
-        folder = type(self).__name__
-        with tables.open_file(path, 'w') as f:
-            group = f.create_group(f.root, folder)
-            im = f.create_carray(group, 'imgs_stim', obj=self.imgs_stim, filters=filters)
-            im.attrs.img_scale = self.img_scale
-            im.attrs.img_dim = self.img_dim
-            f.create_carray(group, 'S', obj=self.S, filters=filters)
-        self.istim.to_hdf(path, f'{folder}/istim')
-        self.pos.to_hdf(path, f'{folder}/pos')
+    def save_processed(self, path: Path_str = 'data/processed.hdf5', overwrite=False):
+        self.imgs_stim  # Process everything.
+        self.S
+        arrs = ['_imgs_stim', '_S']
+        dfs = ['istim', 'pos']
+        params = ['img_scale', 'img_dim']
+        return hdf5_save_from_obj(path, 'SpikeLoader', self, arrs=arrs, dfs=dfs, params=params, overwrite=overwrite)
 
     @classmethod
-    def from_hdf5(cls, path='data/processed.hdf5'):
-        folder = 'SpikeLoader'
-        with tables.open_file(path, 'r') as f:
-            _imgs_stim = f.root.SpikeLoader.imgs_stim.read()
-            img_scale = f.root.SpikeLoader.imgs_stim.attrs.img_scale
-            _img_dim = f.root.SpikeLoader.imgs_stim.attrs.img_dim
-            _S = f.root.SpikeLoader.S.read()
-        istim = pd.read_hdf(path, f'{folder}/istim')
-        pos = pd.read_hdf(path, f'{folder}/pos')
-        del folder, f
-        return cls(**{k: v for k, v in locals().items() if k != 'cls'})
+    def from_hdf5(cls, path: Path_str = 'data/processed.hdf5'):
+        arrs = ['imgs', 'spks', '_imgs_stim', '_S']
+        dfs = ['istim', 'pos']
+        params = ['img_scale', 'img_dim']
+        return cls(path=path, **hdf5_load(path, 'SpikeLoader', arrs=arrs, dfs=dfs, params=params))
 
         # with zarr.open(path, mode='w') as root:
         #     pos = root.create_group('pos')
