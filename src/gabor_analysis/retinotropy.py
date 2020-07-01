3e95b41b6a86ef487412fcdfa59795cd086d0c16
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-07-01 16:36:34 -0400

    Fix change of params name.

--- src/gabor_analysis/retinotropy.py
+++ src/gabor_analysis/retinotropy.py
@@ -21,13 +21,13 @@ axs = axs.flatten()
 params_name = ['σ', 'θ', 'λ', 'γ', 'φ', 'pos_x', 'pos_y']
 for i in range(6):
     if i != 2:
-        u = axs[i].scatter(f.xpos, f.ypos, s=2 * g.corr, c=g.params[:, i], cmap='viridis', alpha=0.9)
+        u = axs[i].scatter(f.xpos, f.ypos, s=2 * g.corr, c=g.params_fit[:, i], cmap='viridis', alpha=0.9)
         axs[i].grid(0)
         axs[i].set_title(params_name[i])
         fig.colorbar(u, ax=axs[i])
 
 # Excluding x pos due to space limitations.
-u = axs[2].scatter(f.xpos, f.ypos, s=2 * g.corr, c=g.params[:, -1], cmap='viridis', alpha=0.9)
+u = axs[2].scatter(f.xpos, f.ypos, s=2 * g.corr, c=g.params_fit[:, -1], cmap='viridis', alpha=0.9)
 axs[2].grid(0)
 axs[2].set_title(params_name[-1])
 fig.colorbar(u, ax=axs[2])
@@ -47,6 +47,6 @@ def make_hexplot(var, name, title, save=None):
         plt.savefig(save)
     plt.show()
 
-make_hexplot(g.params[:, 0], 'σ', 'Size of Gabor Fit (σ) vs y-position')
-make_hexplot(g.params[:, -2], 'Gabor x-pos', 'Retinotropic x-pos of Gabor fit vs physical y-pos')
+make_hexplot(g.params_fit[:, 0], 'σ', 'Size of Gabor Fit (σ) vs y-position')
+make_hexplot(g.params_fit[:, -2], 'Gabor x-pos', 'Retinotropic x-pos of Gabor fit vs physical y-pos')
 make_hexplot(g.corr, 'Gabor Fit Correlation', 'Gabor fit correlation with RF vs physical y-pos')
