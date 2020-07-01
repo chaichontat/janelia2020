4e6a4036af89db1970d8d6f6d3fcc376c34def2d
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-07-01 18:53:14 -0400

    Add canonical ridge test.

--- src/canonical_analysis/canonical_ridge.py
+++ src/canonical_analysis/canonical_ridge.py
@@ -1,17 +1,15 @@
-import pickle
-from pathlib import Path
 from typing import Tuple, Union
 
 import jax.numpy as np
 import matplotlib.pyplot as plt
 import numpy as onp
-import seaborn as sns
 from jax.numpy.linalg import inv, cholesky
 from sklearn.decomposition import PCA
 
 from ..analyzer import Analyzer
 from ..receptive_field.rf import ReceptiveField
 from ..spikeloader import SpikeLoader
+from ..utils.utils import hdf5_save
 
 Arrays = Union[np.DeviceArray, onp.ndarray]
 
@@ -26,13 +24,22 @@ class CanonicalRidge(Analyzer):
 
     """
 
-    def __init__(self, n: int = 25, lx: float = 0.85, ly: float = 0.85, seed: int = 87):
+    params = ['n', 'lambda_x', 'lambda_y', 'seed']
+    arrs = ['V', 'U', 'singular_values', 'coef', 'transformed_U', 'transformed_V']
+    dfs = None
+
+    def __init__(self, n: int = 25, lambda_x: float = 0.85, lambda_y: float = 0.85, seed: int = 87,
+                 V=None, singular_values=None, U=None, coef=None, transformed_U=None, transformed_V=None):
         self.n = n
-        self.λx = lx
-        self.λy = ly
-        self.V, self.singular_values, self.Σ, self.U = 4 * [None]
-        self.coef = np.empty(0)
+        self.lambda_x = lambda_x
+        self.lambda_y = lambda_y
         self.seed = seed
+
+        self.V, self.singular_values, self.U = V, singular_values, U
+        self.Σ = self.singular_values
+        self.coef = coef
+        self.transformed_U, self.transformed_V = transformed_U, transformed_V
+
 
     def fit(self, X: Arrays, Y: Arrays):
         assert X.shape[0] == Y.shape[0]
@@ -52,36 +59,37 @@ class CanonicalRidge(Analyzer):
         return self.transform(X, Y)
 
     def transform(self, X: Arrays, Y: Arrays) -> Tuple[np.DeviceArray, np.DeviceArray]:
-        return X @ self.U, Y @ self.V
+        self.transformed_U, self.transformed_V = X @ self.U, Y @ self.V
+        return self.transformed_U, self.transformed_V
 
     def _calc_canon_mat(self, X: Arrays, Y: Arrays) -> np.DeviceArray:
         X, Y = np.asarray(X), np.asarray(Y)
 
         X -= np.mean(X, axis=0)
         Y -= np.mean(Y, axis=0)
-        K = inv(cholesky((1 - self.λx) * np.cov(X, rowvar=False) + self.λx * np.eye(X.shape[1]))) @ \
+        K = inv(cholesky((1 - self.lambda_x) * np.cov(X, rowvar=False) + self.lambda_x * np.eye(X.shape[1]))) @ \
             (X.T @ Y) / (X.T.shape[0] * Y.shape[1]) @ \
-            inv(cholesky((1 - self.λy) * np.cov(Y, rowvar=False) + self.λy * np.eye(Y.shape[1])))
+            inv(cholesky((1 - self.lambda_y) * np.cov(Y, rowvar=False) + self.lambda_y * np.eye(Y.shape[1])))
 
         return K
 
     def calc_canon_coef(self, X: Arrays, Y: Arrays) -> np.DeviceArray:
-        X_p, Y_p = self.transform(X, Y)
+        if self.transformed_U is None or self.transformed_V is None:
+            self.transform(X, Y)
+        X_p, Y_p = self.transformed_U, self.transformed_V
         return np.array([onp.corrcoef(X_p[:, i], Y_p[:, i])[0, 1] for i in range(self.n)])
 
     def subtract_canon_comp(self, X: Arrays, Y: Arrays) -> Tuple[np.DeviceArray, np.DeviceArray]:
-        X_comp, Y_comp = self.transform(X, Y)
-        return X - (X_comp @ self.U.T), Y - (Y_comp @ self.V.T)
+        if self.transformed_U is None or self.transformed_V is None:
+            self.transform(X, Y)
+        return X - (self.transformed_U @ self.U.T), Y - (self.transformed_V @ self.V.T)
 
 
-if __name__ == '__main__':
-    sns.set()
-    loader = SpikeLoader.from_npz()
-
+def make_regression_truth():
+    loader = SpikeLoader.from_hdf5('tests/data/processed.hdf5')
     print('Running CCA.')
     V1, V2 = loader.S[:, loader.pos['y'] >= 210], loader.S[:, loader.pos['y'] < 210]
     cca = CanonicalRidge().fit(V1, V2)
-    Path('cc.pk').write_bytes(pickle.dumps(cca, protocol=5))
 
     fig, ax = plt.subplots()
     ax.plot(cca.coef)
@@ -89,9 +97,11 @@ if __name__ == '__main__':
     ax.set_xlabel('CCs')
     ax.set_ylabel('Pearson\'s r')
 
-    # %%
     V1s, V2s = cca.subtract_canon_comp(V1, V2)
     rf_v1 = ReceptiveField(loader.img_dim).fit_pc(loader.imgs_stim, V1s)
     rf_v1.plot_rf()
     rf_v2 = ReceptiveField(loader.img_dim).fit_pc(loader.imgs_stim, V2s)
     rf_v2.plot_rf()
+
+    hdf5_save('tests/data/regression_test_data.hdf5', 'CanonicalRidge',
+              arrs={'V1': rf_v1.rf_, 'V2': rf_v2.rf_}, append=True, overwrite_node=True)
---
+++ tests/test_canonical_analysis.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from src.canonical_analysis.canonical_ridge import CanonicalRidge
+from src.receptive_field.rf import ReceptiveField
+from src.spikeloader import SpikeLoader
+from src.utils.utils import hdf5_load
+
+
+def test_regression_canonical_ridge():
+    loader = SpikeLoader.from_hdf5('tests/data/processed.hdf5')
+    V1, V2 = loader.S[:, loader.pos['y'] >= 210], loader.S[:, loader.pos['y'] < 210]
+    cca = CanonicalRidge().fit(V1, V2)
+
+    V1s, V2s = cca.subtract_canon_comp(V1, V2)
+    rf_v1 = ReceptiveField(loader.img_dim).fit_pc(loader.imgs_stim, V1s)
+    rf_v2 = ReceptiveField(loader.img_dim).fit_pc(loader.imgs_stim, V2s)
+
+    gnd = hdf5_load('tests/data/regression_test_data.hdf5', 'CanonicalRidge', arrs=['V1', 'V2'])
+    assert np.max((rf_v1.rf_ - gnd['V1']) / gnd['V1']) < 0.05
+    assert np.max((rf_v2.rf_ - gnd['V2']) / gnd['V2']) < 0.05
