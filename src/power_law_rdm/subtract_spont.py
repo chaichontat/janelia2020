b4c7bbf280d89c252b5beb1323002cf355ff0630
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-08-04 11:32:51 -0400

    Fix z-score in subtract spont.

--- src/power_law_rdm/subtract_spont.py
+++ src/power_law_rdm/subtract_spont.py
@@ -17,20 +17,18 @@ class SubtractSpontAnalyzer(Analyzer):
         super().__init__(**kwargs)
         self.n_pc = n_pc
         self.seed = seed
+        self.pca: PCA
         assert self.n_pc > 0
 
     def fit(self, spks: np.ndarray, idx_spont: np.ndarray) -> SubtractSpontAnalyzer:
-        sponts = zscore(spks[idx_spont, :], axis=0)
-
-        n_used = min(2 * self.n_pc, *sponts.shape)  # Randomized SVD.
-        pca_spont = PCA(n_components=n_used, random_state=np.random.RandomState(self.seed)).fit(
+        sponts = spks[idx_spont, :]
+        
+        self.pca = PCA(n_components=self.n_pc, random_state=np.random.RandomState(self.seed)).fit(
             sponts
         )  # time x neu
-        self.pcs = pca_spont.components_.T[:, : self.n_pc]  # neu x n_components
+        self.pcs = self.pca.components_.T  # neu x n_components
         return self
 
     def transform(self, S: np.ndarray) -> np.ndarray:
-        mean = np.mean(S, axis=0)
-        S -= mean
         proj_spont = S @ self.pcs  # neu x n_components
-        return zscore(S - proj_spont @ self.pcs.T, axis=0) + mean
+        return S - proj_spont @ self.pcs.T
