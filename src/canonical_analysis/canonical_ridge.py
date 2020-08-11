e20b636990a1777ac05e2c0ad27723e171cc65a1
Chaichontat Sriworarat <chaichontat.s@duke.edu>
2020-08-11 16:25:11 -0400

    Add docs to canonical_ridge.

--- src/canonical_analysis/canonical_ridge.py
+++ src/canonical_analysis/canonical_ridge.py
@@ -1,22 +1,18 @@
 from typing import Tuple, Union
 
 import jax.numpy as np
-import matplotlib.pyplot as plt
 import numpy as onp
+
 from jax.numpy.linalg import inv, cholesky
 from sklearn.decomposition import PCA
 
 from ..analyzer import Analyzer
-from ..receptive_field.rf import ReceptiveField
-from ..spikeloader import SpikeLoader
-from ..utils.io import hdf5_save
 
 Arrays = Union[np.DeviceArray, onp.ndarray]
 
 
 class CanonicalRidge(Analyzer):
-    """
-    Class for canonical correlation analysis with ridge regularization.
+    """Class for canonical correlation analysis with ridge regularization.
     See http://www2.imm.dtu.dk/pubdb/edoc/imm4981.pdf
 
     Uses JAX for faster numerical computation.
@@ -24,11 +20,18 @@ class CanonicalRidge(Analyzer):
 
     """
 
-    HYPERPARAMS = ['n', 'lambda_x', 'lambda_y', 'seed']
-    ARRAYS = ['V', 'U', 'singular_values', 'coef', 'transformed_U', 'transformed_V']
+    HYPERPARAMS = ["n", "lambda_x", "lambda_y", "seed"]
+    ARRAYS = ["V", "U", "singular_values", "coef", "transformed_U", "transformed_V"]
     DATAFRAMES = None
 
-    def __init__(self, n: int = 25, lambda_x: float = 0.85, lambda_y: float = 0.85, seed: int = 87, **kwargs):
+    def __init__(
+        self,
+        n: int = 25,
+        lambda_x: float = 0.85,
+        lambda_y: float = 0.85,
+        seed: int = 87,
+        **kwargs
+    ) -> None:
         super().__init__(**kwargs)
         self.n = n
         self.lambda_x = lambda_x
@@ -39,7 +42,9 @@ class CanonicalRidge(Analyzer):
         assert X.shape[0] == Y.shape[0]
 
         K = self._calc_canon_mat(X, Y, [self.lambda_x, self.lambda_y])
-        model = PCA(self.n, random_state=onp.random.RandomState(self.seed)).fit(K)  # Randomized SVD.
+        model = PCA(self.n, random_state=onp.random.RandomState(self.seed)).fit(
+            K
+        )  # Randomized SVD.
 
         # Based on X = UΣV^T.
         self.V = model.components_.T
@@ -60,13 +65,16 @@ class CanonicalRidge(Analyzer):
     @staticmethod
     def _calc_canon_mat(X: Arrays, Y: Arrays, λs) -> np.DeviceArray:
         # https://stackoverflow.com/questions/15670094/speed-up-solving-a-triangular-linear-system-with-numpy
-        K = inv(cholesky(CanonicalRidge._ridge_cov(X, λs[0]))) @ \
-            (X.T @ Y) / (X.T.shape[0] * Y.shape[1]) @ \
-            inv(cholesky(CanonicalRidge._ridge_cov(Y, λs[1])))
+        K = (
+            inv(cholesky(CanonicalRidge._ridge_cov(X, λs[0])))
+            @ (X.T @ Y)
+            / (X.T.shape[0] * Y.shape[1])
+            @ inv(cholesky(CanonicalRidge._ridge_cov(Y, λs[1])))
+        )
         return K
 
     @staticmethod
-    def _ridge_cov(X, λ):
+    def _ridge_cov(X, λ) -> np.DeviceArray:
         cov = (1 - λ) * np.cov(X, rowvar=False)
         cov = cov.at[np.diag_indices(cov.shape[0])].add(λ)
         return cov
@@ -75,7 +83,9 @@ class CanonicalRidge(Analyzer):
         X_t, Y_t = self.transform(X, Y)
         return np.array([onp.corrcoef(X_t[:, i], Y_t[:, i])[0, 1] for i in range(self.n)])
 
-    def subtract_canon_comp(self, X: Arrays, Y: Arrays) -> Tuple[np.DeviceArray, np.DeviceArray]:
+    def subtract_canon_comp(
+        self, X: Arrays, Y: Arrays
+    ) -> Tuple[np.DeviceArray, np.DeviceArray]:
         if self.transformed_U is None or self.transformed_V is None:
             self.transform(X, Y)
         return X - (self.transformed_U @ self.U.T), Y - (self.transformed_V @ self.V.T)
